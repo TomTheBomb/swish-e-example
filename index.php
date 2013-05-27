5a8b22421da99451132de16d3e55cc7631c4af2e
Tom Rothwell <tom_1_29@hotmail.com>
2013-05-27 11:15:44 +1000

    Base of doc search.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Swish-E Example</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+    
+			</div>
+			<div class="span10">
+    				<h1>Swish-E <strong>/</strong> <small><a href="upload.php">Upload a File</a></small></h1>
+				
+				<form name="search" method="post" action="">
+                                        <fieldset>
+
+				<?php 
+        			if (!empty($_POST)) {
+                			$h = new Swish("config/index.swish-e");
+                			$results = $h->query($_POST['term']);
+                			//var_dump($results->getParsedWords("config/index.swish-e"));
+					echo "There are ", $results->hits, ' on ' . $_POST['term'] . '<br />';
+                			while ($r = $results->nextResult()) {
+                        			$file = file_get_contents($r->swishdocpath);
+						$regex = '/([A-Za-z0-9.,-]+\s+){0,5}' . $_POST['term'] . '(\s+[A-Za-z0-9.,-]+){0,5}/i';
+						preg_match($regex, $file, $matches);
+						printf("in file %s, relevance %d <br />",
+                                			$r->swishdocpath,
+                                			$r->swishrank
+                        			);
+						if (!empty($matches)) {
+							echo '<quote>' . $matches[0] . '</quote>';
+						}
+                			}
+        			}
+				?>
+    						<legend>Search file base</legend>
+						<input name="term" type="text" class="input-large">
+						<span class="help-block">OR, AND, * can be used in combination to find terms</span>
+    						<button type="submit" class="btn">Search</button>
+  					</fieldset>
+				</form>
+			</div>
+  		</div>
+	</div>
+	<script src="http://code.jquery.com/jquery.js"></script>
+   	<script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ upload.php
@@ -0,0 +1,54 @@
+<?php
+// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+// of $_FILES.
+if (!empty($_POST)) {
+$uploaddir = '/var/www/swish/files/';
+$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+	echo "File is valid, and was successfully uploaded.\n";
+	echo exec("cd /var/www/swish/config && swish-e -c swish.conf", $return);
+} else {
+	echo "Possible file upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+
+print "</pre>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Swish-E Example</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+        <div class="container-fluid">
+                <div class="row-fluid">
+                        <div class="span2">
+
+                        </div>
+                        <div class="span10">
+                                <h1>Swish-E <strong>/</strong> <small><a href="index.php">Search file base</a></small></h1>
+				<form action="upload.php" method="post" enctype="multipart/form-data">
+                                        <fieldset>
+                                                <legend>Upload a file</legend>
+                                                <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
+                                                <input type="file" name="file" id="file">
+                                                <span class="help-block">Supported formats: pdf, doc, docx, txt</span>
+                                                <button type="submit" class="btn">Submit</button>
+                                        </fieldset>
+                                </form>
+                        </div>
+                </div>
+        </div>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
